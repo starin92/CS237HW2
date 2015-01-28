da1c98d17c39d4cdfe80842e7feb55b2b9089789
starin92 <starin92@users.noreply.github.com>
2015-01-27 20:48:42 -0800

    Update evalAST.js
    All but delay/force working

--- evalAST.js
+++ evalAST.js
@@ -1,111 +1,144 @@
 F.evalAST = function(ast) {
   var env = [null,null,null];
-  return ev(env,ast);
+  return ev(env,ast)[1];
 };
 
 function ev(env, ast) {
   if (typeof ast === "number" || typeof ast === "boolean" || typeof ast === "string" || ast === null) {
-    return ast;
+    return [env,ast];
   } else {
     var tag = ast[0];
     switch (tag) {
         case "+":
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"number");
-          return a1 + a2;
+          return [env, a1 + a2];
         case "-":
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"number");
-          return a1 - a2;
+          return [env, a1 - a2];
         case "*":          
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"number");
-          return a1 * a2;
+          return [env, a1 * a2];
         case "/":
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"number");        
-          return a1 / a2;
+          return [env, a1 / a2];
         case "%":
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"number");        
-          return a1 % a2;
+          return [env, a1 % a2];
         case "=":
-          return ev(env,ast[1]) === ev(env,ast[2]);
+          return [env, ev(env,ast[1])[1] === ev(env,ast[2])[1]];
         case "!=":
-          return ev(env,ast[1]) !== ev(env,ast[2]);
+          return [env, ev(env,ast[1])[1] !== ev(env,ast[2])[1]];
         case "<":
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"number");        
-          return a1 < a2;
+          return [env, a1 < a2];
         case ">":
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"number");        
-          return a1 > a2;
+          return [env, a1 > a2];
         case "and":
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"boolean");          
-          return a1 && a2;
+          return [env, a1 && a2];
         case "or":
-          var a1 = ev(env,ast[1]);
-          var a2 = ev(env,ast[2]);
+          var a1 = ev(env,ast[1])[1];
+          var a2 = ev(env,ast[2])[1];
           checkArgs(a1,a2,"boolean");        
-          return a1 || a2;
+          return [env, a1 || a2];
         case "if":
-          var cond = ev(env,ast[1]);
+          var cond = ev(env,ast[1])[1];
           if(typeof cond !== "boolean"){
             throw new Error("if requires boolean");
           }
-          return cond ? ev(env,ast[2]) : ev(env,ast[3]);
+          return [env, cond ? ev(env,ast[2])[1] : ev(env,ast[3])[1]];
         case "id":
           var c_env=env;
           while(c_env[2]!==null){
             if(c_env[0]===ast[1]){
-              return c_env[1]; 
+              return [env, c_env[1]]; 
             }
             c_env=c_env[2];
           }
           throw new Error("undeclared id:"+ast[1]);
         case "let":
-          var id=ev(env,ast[1]);
-          var val=ev(env,ast[2]);
+          var id=ev(env,ast[1])[1];
+          var val=ev(env,ast[2])[1];
           var newenv = [id,val,env];
           return ev(newenv,ast[3]);
         case "fun":
           var args=ast[1];
           var body=ast[2];
-          return ["closure",args,body,env];
+          return [env,["closure",args,body,env]];
         case "call":
-          var f=ev(env,ast[1]);
+          var f=ev(env,ast[1])[1];
+          var index=-1;
+          var newenv;
+          var val;
+          var id;
           if(f[0]!=="closure"){
             throw new Error("call on non-function type");
           }
           var args = ast.slice(2);
-          if(f[1].length!==args.length){
+          /*if(f[1].length<args.length){
             throw new Error("wrong number of arguments");
-          } 
+          }*/
+
+          //curry time
+          if(args.length<f[1].length){
+            for(var i=0;i<args.length;i++){
+              for(index++;index<f[1].length;index++){
+                if(!isPrim(f[1][index])) break;
+              }
+              val=ev(env,args[i])[1];
+              id=f[1][index];
+              f[1][index]=val;
+              f[3]=[id,val,f[3]];
+            }
+            if(index<f[1].length-1){
+              return [env,f];
+            }
+          }
+
           //make env = arguments env -> closure env
-          var newenv=f[3];
+          newenv=f[3];
           var aval;
           for(var i=0;i<args.length;i++){
-            aval=ev(env,args[i]);
+            if(typeof args[i] === "number" || typeof args[i] === "boolean"){
+              aval=args[i];
+            } else {
+              aval=ev(env,args[i])[1];
+            }
             newenv=[f[1][i],aval,newenv];
           }
-          return ev(newenv,f[2]);
+          var c_env=env;
+          while(c_env[2]!==null){
+            if(c_env[1]===f){
+              newenv=[c_env[0],c_env[1],newenv];
+              break;
+            }
+            c_env=c_env[2];
+          }
+          return [env, ev(newenv,f[2])[1]];
         case "cons":
-          return ["cons",ev(env,ast[1]),ev(env,ast[2])];
+          return [env,["cons",ev(env,ast[1])[1],ev(env,ast[2])[1]]];
         case "seq":
-          ev(env,ast[1]);
-          return ev(env,ast[2]);
+          newenv=ev(env,ast[1])[0];
+          return ev(newenv,ast[2]);
         case "match":
-          var p=ev(env,ast[1]);
+          var p=ev(env,ast[1])[1];
           var patterns = ast.slice(2);
           for(var i=0;i<patterns.length;i+=2){
             var newenv = matchRec(env,p,patterns[i]);
@@ -115,13 +148,53 @@ function ev(env, ast) {
           throw new Error("match failure");
         case "set":
           var c_env=env;
+          var head=c_env;
           while(c_env[2]!==null){
             if(c_env[0]===ast[1]){
-              return c_env[1]; 
+              var val=ev(env,ast[2])[1];
+              c_env[1]=val;
+              return [head,val];
             }
             c_env=c_env[2];
           }
           throw new Error("set on undeclared id:"+ast[1]);
+        case "listComp":
+          var expr=ast[1];
+          var id=ast[2];
+          var list=ev(env,ast[3])[1];
+          var outList;
+          var head=null;
+          var newenv;
+          var val;
+          var pred;
+          if(ast.length===5){
+            pred=ast[4];
+          }
+          while(list!==null){
+            newenv=[id,list[1],env];
+            if(ast.length===5){
+              if(!ev(newenv,pred)[1]){
+                list=list[2];
+                continue;
+              }
+            }
+            val=["cons",ev(newenv,expr)[1],null];
+            if(outList===undefined){
+              outList=val;
+              head=outList;
+            }else{
+              outList[2]=val;
+              outList=outList[2];
+            }
+            list=list[2];
+          }
+          return [env,head];
+        case "delay":
+          //var e=ev(env,ast[1])[1];
+          return [env,["closure",[],ast[1],env]];
+        case "force":
+          //var e=ev(env,ast[1])[1];
+          return [env,ev(env,["call",ast[1]])[1]];
            
     }
   }
@@ -132,6 +205,12 @@ function checkArgs(ast1,ast2,type){
     throw new Error("incorrect type to operator");
   }
   return;
+}
+
+function isPrim(t){
+  if(typeof t === "boolean" || typeof t === "number"){
+    return true;
+  } else return false;
 }
 
 function matchRec(env,matchExpr,pattern){
@@ -148,18 +227,30 @@ function matchRec(env,matchExpr,pattern){
       return "no";
     }
   } else if(pattern[0]==="_"){
-    if(typeof matchExpr ==="number" || typeof matchExpr ==="boolean"){
+    return env;
+    if(matchExpr ===null || typeof matchExpr ==="number" || typeof matchExpr ==="boolean"){
+      return env;
+    } else if(matchExpr[0]==="cons"){
       return env;
     } else {
       return "no";
     }
   } else if(pattern[0]==="id") {
-    if(typeof matchExpr ==="number" || typeof matchExpr ==="boolean"){
+    return [pattern[1], matchExpr, env];
+    /*
+    if(matchExpr === null){
+      return [pattern[1], matchExpr, env];
+    } else if(typeof matchExpr ==="number" || typeof matchExpr ==="boolean"){
+      return [pattern[1], matchExpr, env];
+    } else if(matchExpr[0]==="cons" || matchExpr[0]==="closure"){
       return [pattern[1], matchExpr, env];
     } else {
       return "no";
-    }
+    }*/
   } else if(pattern[0]==="cons"){
+    if(matchExpr===null){
+      return "no";
+    }
     if(matchExpr[0]!=="cons"){
       return "no";
     } else {
